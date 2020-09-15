4444c8325081a9a229fe085e7913265c9f7848f6
Vitor de Souza <vitordesolza@gmail.com>
2020-09-15 10:19:16 -0300

    adicionando modelo Todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: String = "Minhas tarefas";
+
+  constructor() {
+    this.todos.push(new Todo(1, "Tarefa 1", false));
+    this.todos.push(new Todo(2, "Tarefa 2", false));
+    this.todos.push(new Todo(3, "Tarefa 3", true));
+  }
+
+  remove(todo: Todo){
+    const index = this.todos.indexOf(todo);
+    if(index !== -1){
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo){
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo){
+    todo.done = false;
+  }
 }
